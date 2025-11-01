f9c401c27ece764536aeeaaa2d706395de0f422a
Filip Kubala <69809370+Luckownia@users.noreply.github.com>
2025-11-01 17:15:04 +0100

    Delete captiveportal directory

--- captiveportal/switch_to_client.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Przełączanie w tryb klienta Wi-Fi..."
-sudo systemctl stop hostapd
-sudo systemctl stop dnsmasq
-sudo wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
-sudo dhclient wlan0
-echo "Połączono z siecią."
-
-# uprawnienia nadać chmod +x switch_to_client.sh
